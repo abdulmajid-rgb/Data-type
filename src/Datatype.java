0489732f0cbd1a62b1c7866400a2f5efc5df21d1
Abdulmajid-rgb <eabdulmejid@gmail.com>
2020-06-15 17:11:16 -0400

    DataTypr example

---
+++ src/Datatype.java
@@ -0,0 +1,19 @@
+public class Datatype {
+
+    public static void main(String[] args) {
+        boolean isSkyeBlue = true;
+        byte b = -128;
+        char myChar = 'a';
+        short myShortVariable = -3000;
+        int myInteger = 9000;
+        float myFloat = 7.42f;
+        double myDouble = 90.28;
+        long longVariable = 999999;
+        System.out.println("My boolean Variable : " + isSkyeBlue);
+        System.out.println("My byte variable is equal  to " + -128);
+        System.out.println("MyChar Variable is equal to " + myChar);
+        System.out.println("Short Variable is equal to " + myShortVariable);
+        System.out.println("My integer plus 1k is equal to " + (myInteger + 1000));
+    }
+}
+
